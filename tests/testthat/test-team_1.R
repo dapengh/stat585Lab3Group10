e343eb6049ebc8fa9d29c0811ca15be883e18581
yonghui huo <42651437+huo198909@users.noreply.github.com>
2019-03-24 17:47:33 -0500

    Add files via upload

---
+++ tests/testthat/test-team_1.R
@@ -0,0 +1,11 @@
+context("test-team_1")
+
+test_that("output is a data frame",{
+  expect_s3_class(team_1(ozbig), "data.frame")
+})
+
+
+test_that("only takes correct shapefiles", {
+  expect_error(team_1(ozbig, tolerance = 0))
+  expect_error(team_1(ozbig, tolerance = -1))
+})
